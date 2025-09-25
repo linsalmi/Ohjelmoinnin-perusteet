da5728486262c6e82bfc05f2ca4082a163ac393c
Linda Salmi <linda.salmi@gmail.com>
2025-09-25 12:23:30 +0300

    Lisätty tehtävä 7

--- Week2/A2_T7.py
+++ Week2/A2_T7.py
@@ -6,6 +6,12 @@
 # Example program run:
 
 # Program starting.
+print("Program starting.")
 # Insert fahrenheits: 50
+Fahr = float(input("Insert fahrenheits: "))
+Celc = (Fahr - 32) / 1.8
+Celc = round(Celc, 1)
 # 50.0°F is 10.0°C
+print(f"{Fahr}°F is {Celc}°C")
 # Program ending.
+print("Program ending.")
