7395c39351fb7d511bdc22324f34c5d13e6ee6a5
Linda Salmi <linda.salmi@gmail.com>
2025-10-01 23:24:59 +0300

    Lisätty tehtävä 7

--- Week3/A3_T7.py
+++ Week3/A3_T7.py
@@ -24,16 +24,45 @@
 # “Unknown option.”
 # Example program runs
 
-# run 1 run 2 run 3 run 4 run 5 run 6
-# Program starting.
-# Testing decision structures.
-# Insert an integer: 250
-# Options:
-# 1 - In one multi-branched decision
-# 2 - In multiple independent if-statements
-# 0 - Exit
-# Your choice: 1
-# Using one multi-branched decision structure.
-# Result is 272
 
+# Program starting.
+print("Program starting.")
+# Testing decision structures.
+print("Testing decision structures.")
+# Insert an integer: 250
+numero = int(input("Insert an integer: "))
+# Options:
+print("Options:")
+# 1 - In one multi-branched decision
+print("1 - In one multi-branched decision")
+# 2 - In multiple independent if-statements
+print("2 - In multiple independent if-statements")
+# 0 - Exit
+print("0 - Exit")
+# Your choice: 1
+valinta = int(input("Your choice: "))
+if valinta == 1:
+# Using one multi-branched decision structure.
+    print("Using one multi-branched decision structure.")
+# Result is 272
+    if numero >= 400:
+        print(f"Result is {numero + 44}")
+    elif numero >= 200:
+        print(f"Result is {numero + 22}")
+    elif numero >= 100:
+        print(f"Result is {numero + 11}")
+elif valinta == 2:
+    print("Using multiple independent if-statements structure.")
+    if numero >= 400:
+        numero = numero + 44
+    if numero >= 200:
+        numero = numero + 22
+    if numero >= 100:
+        numero = numero + 11
+    print(f"Result is {numero}")
+elif valinta == 0:
+    print("Exiting...")
+else:
+    print("Unknown option.")
 # Program ending.
+print("\nProgram ending.")
